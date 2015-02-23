19414ddb7124ebbfdf1152d753ccb8692f641df9
justin <justin@localhost.localdomain>
2015-02-23 10:54:09 -0700

    Support building with FastBit >= 1.3.9

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-all:
-	sh get_and_build_fastbit.sh
+all: fastbit udf
+
+fastbit: 
+	sh get_and_build_fastbit.sh "2.0.1"
+	
+udf: fb_udf.cpp
 	g++ --std=c++11 -shared -Wl,-rpath '-Wl,$$ORIGIN' -L lib/ -I include/ -I `mysql_config --include` -l fastbit -o fb_udf.so `mysql_config --cxxflags` fb_udf.cpp
 
 install:
@@ -7,4 +11,9 @@ install:
 	cp lib/*.so* `mysql_config --plugindir`
 
 clean:
-	rm *.so
+	rm -f *.so
+
+deep_clean:
+	rm -f *.so *.gz
+	rm -rf lib/ include/ bin/ fastbit-*/ share/
+	
--- get_and_build_fastbit.sh
+++ get_and_build_fastbit.sh
@@ -1,19 +1,65 @@
-FASTBIT_VERSION="2.0.1"
- 
-if [ ! -e "fastbit-2.0.1.tar.gz" ]
+FASTBIT_REMOTE_DIR=0
+FASTBIT_VERSION=$1
+
+##YOU SHOULD NOT NEED TO MODIFY BELOW HERE (unless you are using version > 2.0.1)
+########################################################################################################
+if [ "" = "$FASTBIT_VERSION" ]
 then
-  wget https://codeforge.lbl.gov/frs/download.php/415/fastbit-"$FASTBIT_VERSION".tar.gz || exit 1
+  FASTBIT_VERSION="2.0.1"
+fi
+
+if [ "2.0.1" = "$FASTBIT_VERSION" ] 
+then
+  FASTBIT_PREFIX="fastbit-"
+  FASTBIT_REMOTE_DIR=415
+  FASTBIT_DIR="$FASTBIT_PREFIX-$FASTBIT_VERSION"
+fi
+
+if [ "2.0.0" = "$FASTBIT_VERSION" ] 
+then
+  FASTBIT_PREFIX="fastbit-"
+  FASTBIT_REMOTE_DIR=414
+fi
+
+if [ "1.3.9" = "$FASTBIT_VERSION" ]
+then
+  FASTBIT_PREFIX="fastbit-ibis"
+  FASTBIT_REMOTE_DIR=413
+fi
+
+if [ "0" = "$FASTBIT_REMOTE_DIR" ]
+then
+  echo "unsupported version of FastBit specified.  Please use 1.3.9 or higher"
+  exit 1
+fi
+
+FASTBIT_DIR="$FASTBIT_PREFIX$FASTBIT_VERSION"
+FASTBIT_TARBALL="$FASTBIT_PREFIX$FASTBIT_VERSION.tar.gz"
+FASTBIT_URL="https://codeforge.lbl.gov/frs/download.php/$FASTBIT_REMOTE_DIR/$FASTBIT_TARBALL"
+echo "Getting fastbit with following parameters"
+echo $FASTBIT_VERSION $FASTBIT_URL $FASTBIT_REMOTE_DIR $FASTBIT_DIR $FASTBIT_TARBALL
+ 
+if [ ! -e "$FASTBIT_TARBALL" ]
+then
+  wget $FASTBIT_URL || exit 1
 fi
 
 if [ ! -e "lib" ]
 then 
 
-  if [ ! -e "fastbit-$FASTBIT_VERSION" ]
+  if [ ! -e "$FASTBIT_DIR" ]
   then
-    tar zxovf fastbit-"$FASTBIT_VERSION".tar.gz || exit 1
+    tar zxovf $FASTBIT_TARBALL || exit 1
   fi
 
-  cd fastbit-"$FASTBIT_VERSION"/
+  if [ ! -e "$FASTBIT_DIR/configure" ]
+  then
+    echo "Fastbit download/extract failed!"
+    exit 1
+  fi;
+
+
+  cd $FASTBIT_DIR
 
   if [ ! -e "Makefile" ]
   then
